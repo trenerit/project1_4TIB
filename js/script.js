538c17c38539bc0e586ec08e4a805fa61af7874d
Piotr Kania <piotr@trener-it.pl>
2026-03-24 12:55:37 +0100

    modify project

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// for(let i = 1; i <= 100; i) {
+//     console.log(i);
+// }
+
+let hamburger = document.querySelector('.material-symbols-outlined');
+
+hamburger.addEventListener('click', () => {
+    alert('klik');
+});
