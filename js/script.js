efdff5cf0086b9f38d9d6259f8e4e474cbf17c81
Piotr Kania <piotr@trener-it.pl>
2026-03-25 13:02:13 +0100

    mod project

--- js/script.js
+++ js/script.js
@@ -5,7 +5,25 @@
 // }
 
 let hamburger = document.querySelector('.material-symbols-outlined');
+let close = document.querySelector('.mobile-menu .material-symbols-outlined');
+let menuMobile = document.querySelector('.mobile-menu');
+let menuMobileItems = document.querySelectorAll('.mobile-menu a');
+
+// console.log(menuMobileItems);
+
+menuMobileItems.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        // menuMobile.style.display = "none";
+        menuMobile.classList.add('class-hide');
+    })
+});
 
 hamburger.addEventListener('click', () => {
-    alert('klik');
+    // menuMobile.style.display = "flex";
+    menuMobile.classList.remove('class-hide');
+});
+
+close.addEventListener('click', () => {
+    // menuMobile.style.display = "none";
+    menuMobile.classList.add('class-hide');
 });
